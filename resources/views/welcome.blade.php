49b498f28fb9ec34088f9706146d4a412f9eaba2
LAMIM <lamimlamim1992@gmail.com>
2025-06-15 12:41:36 +0600

    Implement responsive navbar and create initial Post model with fields

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,13 +1,32 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Larning</title>
     <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
-<body>
+
+<body class="bg-gray-100 min-h-screen justify-between ">
+
+    <div class="flex justify-between items-center py-4 bg-white shadow-md rounded-md px-6">
+        <a class="bg-gradient-to-r from-red-400 to-yellow-500 text-white font-bold py-2 px-4 rounded">
+            Home
+        </a>
+
+
+            <a class="bg-gradient-to-r from-green-400 to-blue-500 text-white font-bold py-2 px-4 rounded">
+            Add New Post
+        </a>
+    </div>
+
+
+
+
 
 </body>
+
 </html>
